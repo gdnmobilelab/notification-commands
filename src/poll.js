028d0594ff832f9aa12f48bd0042db0d5ba7afe0
Connor Jennings <connor.jennings@guardian.co.uk>
2016-06-21 18:31:29 -0400

    Adding in api key header

--- src/poll.js
+++ src/poll.js
@@ -3,12 +3,17 @@ const getRegistration = require('./util/get-registration');
 
 module.exports = {
     castVote: function ({url, answerId}) {
+        if (!config.poll || !config.poll.key) {
+            throw new Error("Must set both pushy key and host config variables");
+        }
+        
         getRegistration().pushManager.getSubscription().then((subscription) => {
             return fetch(url, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
-                    'Content-type': 'application/json'
+                    'Content-type': 'application/json',
+                    'x-polls-api-key': config.poll.key
                 },
                 body: JSON.stringify({
                     answerId: answerId,
