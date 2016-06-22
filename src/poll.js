b9e7368340a6f027d5443208dc5dc7b73d4e0004
Connor Jennings <connor.jennings@guardian.co.uk>
2016-06-22 15:12:26 -0400

    Add a command to fetch pull results

--- src/poll.js
+++ src/poll.js
@@ -4,7 +4,7 @@ const config = require('./config');
 module.exports = {
     castVote: function ({url, answerId}) {
         if (!config.poll || !config.poll.key) {
-            throw new Error("Must set both pushy key and host config variables");
+            throw new Error("Must set pull key");
         }
         
         getRegistration().pushManager.getSubscription().then((subscription) => {
@@ -17,6 +17,28 @@ module.exports = {
                 },
                 body: JSON.stringify({
                     answerId: answerId,
+                    user: {
+                        id: subscription.endpoint,
+                        subscription: subscription
+                    }
+                })
+            });
+        });
+    },
+    pullResults: function({url}) {
+        if (!config.poll || !config.poll.key) {
+            throw new Error("Must set pull key");
+        }
+
+        getRegistration().pushManager.getSubscription().then((subscription) => {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json',
+                    'x-polls-api-key': config.poll.key
+                },
+                body: JSON.stringify({
                     user: {
                         id: subscription.endpoint,
                         subscription: subscription
